7ebe37fa5bed6be74f56894a5b1405742e241ea4
Matt Mikuta <mmikuta@hawk.iit.edu>
2016-11-02 03:51:19 -0500

    Fixed compile warning, added README

--- hw6/mpibm.c
+++ hw6/mpibm.c
@@ -127,6 +127,3 @@ static void handle_error(int errcode, char *str)
 	 *    * the following line */
 	MPI_Abort(MPI_COMM_WORLD, 1);
 }
-
-#define MPI_CHECK(fn) { int errcode; errcode = (fn); \
-    if (errcode != MPI_SUCCESS) handle_error(errcode, #fn ); }}
